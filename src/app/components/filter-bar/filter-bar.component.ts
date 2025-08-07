74c0ad799467bc14fe146a70f6d7052b6d0372a8
Nikhithaspoojary16 <nikhithaspoojary16@gmail.com>
2025-08-07 13:01:30 +0200

    Intial commit -new component created

---
+++ src/app/components/filter-bar/filter-bar.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './filter-bar.component.html',
+  styleUrl: './filter-bar.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class FilterBarComponent {
+
+}
